e1f36fd8fec27c6818f6950f70af779182a881f8
advancedtechnoprojects <167116986+advancedtechnoprojects@users.noreply.github.com>
2024-04-15 22:03:47 +0530

    Update install.py

--- install.py
+++ install.py
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
-=======
-# this is mahesh--comment1
-# comment added from github
-# comment added to branch 1
-# comment added to branch2
-# edit in main code
->>>>>>> 06cd84c681cf8bfd34162d56dff0644145800d4e
+# forked comment
 from flask import Flask, render_template, jsonify, request, redirect, url_for
 from flask_socketio import SocketIO
 import hardwareCommunication as hd
